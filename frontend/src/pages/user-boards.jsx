08d1ea6b00ad619a94e97fd8ae393e94dfbf743f
Martin Sh <martinshn@gmail.com>
2021-09-22 22:18:06 +0300

    board list and preview

---
+++ frontend/src/pages/user-boards.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BoardList } from '../cmps/board-list';
+
+export class UserBoards extends React.Component{
+    state={
+        isStar: true,
+    }
+
+    render(){
+        const {isStar} = this.state
+        return(
+            <section className="user-boards">
+                {isStar&&<h3 className="star">Starred boards</h3>}
+                {isStar&&<BoardList isStarred/>}
+                <h3>YOUR BOARDS</h3>
+                <BoardList isAdd/>
+                <h3>GUEST BOARDS</h3>
+                <BoardList />
+            </section>
+        )
+    }
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,15 @@
 import { HomePage } from './pages/home-page';
+import { UserBoards } from './pages/user-boards';
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
   {
     path: '/',
     component: HomePage,
+  },
+  {
+    path: '/board',
+    component: UserBoards,
   },
 ];
 
