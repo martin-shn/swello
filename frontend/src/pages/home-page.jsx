991a12d1e7527f949a8abbccb67a7a2577d6e2d8
Martin Sh <martinshn@gmail.com>
2021-09-23 11:37:33 +0300

    added data to json

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -16,7 +16,7 @@ export const HomePage = () => {
             <h1>Swello helps teams move work forward.</h1>
             <p>Collaborate, manage projects, and reach new productivity peaks.
               From high rises to the home office, the way your team works is unique—accomplish it all with Swello.</p>
-            <Link to="/" className="btn">Get Started-it's free!</Link>
+            <Link to="/board" className="btn">Get Started-it's free!</Link>
           </div>
           <img src={HeroImg} alt="hero" />
         </div>
