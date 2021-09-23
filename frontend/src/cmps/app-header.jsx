624c21bec8897dd66baf9457ab27e8b24d25ce64
Aviv-Yaari <avivyar@gmail.com>
2021-09-23 16:55:49 +0300

    added search isactive state logic

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,41 +1,58 @@
+import React, { Component } from 'react';
 import { Avatar } from '@mui/material';
 import { ReactComponent as ArrowDownIcon } from '../assets/svg/arrow-down.svg';
 import { ReactComponent as NotificationsIcon } from '../assets/svg/notifications.svg';
 import { ReactComponent as SearchIcon } from '../assets/svg/search.svg';
+import { ReactComponent as CloseIcon } from '../assets/svg/close.svg';
 
-export const AppHeader = props => {
-  return (
-    <header
-      className={
-        'app-header flex align-center full with-main-layout' +
-        (props.isUserBoardsPage ? ' user-boards-header' : '')
-      }>
-      <div>
-        <img className="logo" />
-      </div>
-      <div className="actions">
-        <button>
-          <span>Boards</span>
-          <ArrowDownIcon />
-        </button>
-        <button>
-          <span>Starred</span>
-          <ArrowDownIcon />
-        </button>
-        <button className="btn-create">Create</button>
-      </div>
-      <div>
-        <div className="search flex align-center">
-          <span>
-            <SearchIcon />
-          </span>
-          <input type="text" placeholder="Search" />
+export class AppHeader extends React.Component {
+  state = { isSearchActive: false };
+  render() {
+    const { isUserBoardsPage } = this.props;
+    const { isSearchActive } = this.state;
+    return (
+      <header
+        className={
+          'app-header flex align-center full with-main-layout' +
+          (isUserBoardsPage ? ' user-boards-header' : '')
+        }>
+        <div>
+          <img className="logo" />
         </div>
-        <button className="btn-notifications">
-          <NotificationsIcon />
-        </button>
-        <Avatar className="avatar" alt="Guest User" src="/static/images/avatar/3.jpg" />
-      </div>
-    </header>
-  );
-};
+        <div className="actions">
+          <button>
+            <span>Boards</span>
+            <ArrowDownIcon />
+          </button>
+          <button>
+            <span>Starred</span>
+            <ArrowDownIcon />
+          </button>
+          <button className="btn-create">Create</button>
+        </div>
+        <div>
+          <div className={'search flex align-center' + (isSearchActive ? ' active' : '')}>
+            <span>
+              <SearchIcon />
+            </span>
+            <input
+              type="text"
+              placeholder="Search"
+              onFocus={() => this.setState({ isSearchActive: true })}
+              onBlur={() => this.setState({ isSearchActive: false })}
+            />
+            {isSearchActive && (
+              <span style={{ height: '16px' }}>
+                <CloseIcon />
+              </span>
+            )}
+          </div>
+          <button className="btn-notifications">
+            <NotificationsIcon />
+          </button>
+          <Avatar className="avatar" alt="Guest User" src="/static/images/avatar/3.jpg" />
+        </div>
+      </header>
+    );
+  }
+}
