130305c9300b798cb18d4c0c706181ec306cb24b
Martin Sh <martinshn@gmail.com>
2021-09-29 11:30:51 +0300

    fixed bugs

--- frontend/src/cmps/card/card-checklists.jsx
+++ frontend/src/cmps/card/card-checklists.jsx
@@ -5,6 +5,7 @@ import { CardChecklist } from './checklist/card-checklist';
 export class CardChecklists extends Component {
   state = { addingChecklistId: null };
   addBtnRef = React.createRef();
+  card = this.props.card;
 
   onAddingItem = (addingChecklistId, inputRef) => {
     this.setState({ addingChecklistId }, () => {
@@ -13,29 +14,25 @@ export class CardChecklists extends Component {
   };
 
   onAddItem = (checklist, item) => {
-    const { card } = this.props;
-    const updatedCard = cardService.addChecklistItem(card, checklist.id, item);
+    const updatedCard = cardService.addChecklistItem(this.card, checklist.id, item);
     const { checklists } = updatedCard;
     this.props.updateField({ checklists });
   };
 
   onUpdateItem = (checklist, updatedItem) => {
-    const { card } = this.props;
-    const updatedCard = cardService.updateChecklistItem(card, checklist.id, updatedItem);
+    const updatedCard = cardService.updateChecklistItem(this.card, checklist.id, updatedItem);
     const { checklists } = updatedCard;
     this.props.updateField({ checklists });
   };
 
   onRemoveItem = (checklist, itemId) => {
-    const { card } = this.props;
-    const updatedCard = cardService.removeChecklistItem(card, checklist.id, itemId);
+    const updatedCard = cardService.removeChecklistItem(this.card, checklist.id, itemId);
     const { checklists } = updatedCard;
     this.props.updateField({ checklists });
   };
 
   onDeleteChecklist = checklistId => {
-    const { card } = this.props;
-    const updatedCard = cardService.deleteChecklist(card, checklistId);
+    const updatedCard = cardService.deleteChecklist(this.card, checklistId);
     const { checklists } = updatedCard;
     this.props.updateField({ checklists });
   };
@@ -43,6 +40,7 @@ export class CardChecklists extends Component {
   // add checklist logic is in "add_checklist" cmp (popover)
 
   render() {
+    this.card = this.props.card;
     const { checklists, 
       // card 
     } = this.props;
