51de3ac942fd7725cdd696506e9de33a73d36d4c
Aviv-Yaari <avivyar@gmail.com>
2021-09-30 14:17:29 +0300

    fixed ui bugs

--- frontend/src/cmps/card/card-popover-pages/add-location/add-location.jsx
+++ frontend/src/cmps/card/card-popover-pages/add-location/add-location.jsx
@@ -1,43 +1,58 @@
-import React from "react";
-import { RemoveLocation } from "./remove-location";
-import { SaveLocation } from "./save-location";
+import React from 'react';
+import { RemoveLocation } from './remove-location';
+import { SaveLocation } from './save-location';
 export class AddLocation extends React.Component {
-    state = {
-        currPage: this.props.currPage || 'main',
-    }
-    onSetPage = (page) => {
-        this.setState({ currPage: page })
-    }
-    onRemoveLocation = () => {
-        const { updateField } = this.props;
-        updateField({ location: null });
-        this.props.closeCardPopover()
-    };
-    onSaveLocation = (location) => { // gets the full location object
-        const { updateField } = this.props;
-        updateField({ location });
-        this.props.closeCardPopover()
-    }
-    render() {
-        const { card, closeCardPopover, isFromNav } = this.props;
-        const { currPage } = this.state
-        return (
-            <>
-                {currPage === 'main' &&
-                    <>
-                        <div className='popper-header'>
-                            <div>{card.location.title}</div>
-                            <button onClick={closeCardPopover}></button>
-                        </div>
-                        <div className='popper-menu'>
-                            <button>Change location</button>
-                            <button onClick={() => this.onSetPage('remove')}>Remove</button>
-                        </div>
-                    </>
-                }
-                {currPage === 'remove' && <RemoveLocation closeCardPopover={closeCardPopover} onSetPage={this.onSetPage} onRemoveLocation={this.onRemoveLocation} />}
-                {currPage === 'save' && <SaveLocation card={card} closeCardPopover={closeCardPopover} onSetPage={this.onSetPage} onSaveLocation={this.onSaveLocation} isFromNav={isFromNav} />}
-            </>
-        )
-    }
+  state = {
+    currPage: this.props.currPage || 'main',
+  };
+  onSetPage = page => {
+    this.setState({ currPage: page });
+  };
+  onRemoveLocation = () => {
+    const { updateField } = this.props;
+    updateField({ location: null });
+    this.props.closeCardPopover();
+  };
+  onSaveLocation = location => {
+    // gets the full location object
+    const { updateField } = this.props;
+    updateField({ location });
+    this.props.closeCardPopover();
+  };
+  render() {
+    const { card, closeCardPopover, isFromNav } = this.props;
+    const { currPage } = this.state;
+    return (
+      <>
+        {currPage === 'main' && (
+          <section className="add-location">
+            <div className="popper-header">
+              <div>{card.location.title}</div>
+              <button onClick={closeCardPopover}></button>
+            </div>
+            <div className="popper-menu">
+              <button>Change location</button>
+              <button onClick={() => this.onSetPage('remove')}>Remove</button>
+            </div>
+          </section>
+        )}
+        {currPage === 'remove' && (
+          <RemoveLocation
+            closeCardPopover={closeCardPopover}
+            onSetPage={this.onSetPage}
+            onRemoveLocation={this.onRemoveLocation}
+          />
+        )}
+        {currPage === 'save' && (
+          <SaveLocation
+            card={card}
+            closeCardPopover={closeCardPopover}
+            onSetPage={this.onSetPage}
+            onSaveLocation={this.onSaveLocation}
+            isFromNav={isFromNav}
+          />
+        )}
+      </>
+    );
+  }
 }
