2c17d4ab7ae6ca8039c5d89bb9c3aee5b3c65be7
Aviv-Yaari <avivyar@gmail.com>
2021-10-06 18:44:57 +0300

    ui fixes

--- frontend/src/pages/invite-page.jsx
+++ frontend/src/pages/invite-page.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import { onLogin } from '../store/actions/user.actions'
-import { updateBoard, loadBoard } from '../store/actions/board.actions'
-import { userService } from '../services/user.service'
-import { boardService } from '../services/board.service'
-import { LoaderPage } from '../cmps/loader/loader-page'
+import { onLogin } from '../store/actions/user.actions';
+import { updateBoard, loadBoard } from '../store/actions/board.actions';
+import { userService } from '../services/user.service';
+import { boardService } from '../services/board.service';
+import { LoaderPage } from '../cmps/loader/loader-page';
 class _InvitePage extends React.Component {
-    async componentDidMount() {
+    async componentDidMount () {
         const { boardId } = this.props.match.params;
         const user = userService.getLoggedinUser();
-        if (!user) this.props.history.push(`/invite/${boardId}/login`)
-        const board = await boardService.getById(boardId)
+        if (!user) {
+            this.props.history.push(`/invite/${ boardId }/login`);
+            return;
+        }
+        const board = await boardService.getById(boardId);
         if (!board.members.some(member => member._id === user._id)) {
             const miniUser = {
                 _id: user._id,
@@ -18,14 +21,14 @@ class _InvitePage extends React.Component {
                 imgUrl: user.imgUrl,
                 mentions: user.mentions,
                 username: user.username
-            }
-            const updatedBoard = { ...board, members: [...board.members, miniUser] }
-            await this.props.updateBoard(updatedBoard)
+            };
+            const updatedBoard = { ...board, members: [...board.members, miniUser] };
+            await this.props.updateBoard(updatedBoard);
         }
-        this.props.history.push(`/board/${boardId}`)
+        this.props.history.push(`/board/${ boardId }`);
     }
-    render() {
-        return <LoaderPage />
+    render () {
+        return <LoaderPage />;
     }
 }
 
@@ -33,6 +36,6 @@ const mapDispatchToProps = {
     updateBoard,
     onLogin,
     loadBoard
-}
+};
 
-export const InvitePage = connect(null, mapDispatchToProps)(_InvitePage)
+export const InvitePage = connect(null, mapDispatchToProps)(_InvitePage);
