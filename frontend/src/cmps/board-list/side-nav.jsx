58600202f051bb92647bd831493209b3fe9e2822
Martin Sh <martinshn@gmail.com>
2021-10-06 08:00:35 +0300

    login fixed

--- frontend/src/cmps/board-list/side-nav.jsx
+++ frontend/src/cmps/board-list/side-nav.jsx
@@ -1,24 +1,10 @@
 // import React from 'react';
 
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom';
-=======
 import { NavLink, Link } from 'react-router-dom';
->>>>>>> 52000a239b60f052547c14e299eccc63060d03f6
 
 export function SideNav() {
   return (
     <ul>
-<<<<<<< HEAD
-      <li className="boards">
-        <NavLink to="/board">Boards</NavLink>
-      </li>
-      <li className="templates">
-        <NavLink to="/templates">Templates</NavLink>
-      </li>
-      <li className="home">
-        <NavLink to="/">Home</NavLink>
-=======
       <li >
         <NavLink to="/board" className="boards"><span>Boards</span></NavLink>
       </li>
@@ -27,7 +13,6 @@ export function SideNav() {
       </li>
       <li >
         <Link to="/" className="home"><span>Home</span></Link>
->>>>>>> 52000a239b60f052547c14e299eccc63060d03f6
       </li>
       <hr />
     </ul>
