c54164bf8be8abec8e8ebf228c1b1fceab719347
Aviv-Yaari <avivyar@gmail.com>
2021-10-07 18:43:00 +0300

    started work on offline mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,22 +7,19 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './assets/styles/main.scss';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import swDev from './swDev';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <RootCmp />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <RootCmp />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+swDev();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// serviceWorkerRegistration.register();
+
+// reportWebVitals();
